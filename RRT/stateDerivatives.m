6163d025ed75d6174910a3757a48e16d6424ae26
vpan14 <vpan14@gmail.com>
2018-11-03 15:35:48 -0400

    fixed bugs with missing variables

--- RRT/stateDerivatives.m
+++ RRT/stateDerivatives.m
@@ -1,7 +1,8 @@
-function [derivatives] = stateDerivatives(state, length, width, mass, vx, cf, cr, lf, lr, steeringAngle)
+function [derivatives] = stateDerivatives(state, length, width, mass, vx, cf, cr, lf, lr, inertia, steeringAngle)
 
     theta = state(3);
     vy = state(4);
+    r = state(5); %yaw rate or thetadot
     %Values for non-linear model of vydot and r(yaw rate)
     A = -(cf*cos(steeringAngle)+cr)/(mass*vx);
     B = (-lf*cf*cos(steeringAngle)+lr*cr)/(mass*vx)-vx;
@@ -10,10 +11,10 @@ function [derivatives] = stateDerivatives(state, length, width, mass, vx, cf, cr
     E = cf*cos(steeringAngle)/mass;
     F = lf*cf*cos(steeringAngle)/inertia;
     
-    vyDot = A*vy + C*r + E*input;
-    rDot = B*vy + D*r + F*input;
-    xDot = speed*cos(theta) - vy*sin(theta);
-    yDot = speed*sin(theta) + vy*cos(theta);
+    vyDot = A*vy + C*r + E*steeringAngle;
+    rDot = B*vy + D*r + F*steeringAngle;
+    xDot = vx*cos(theta) - vy*sin(theta);
+    yDot = vx*sin(theta) + vy*cos(theta);
     thetaDot = r;
     
     derivatives = [xDot, yDot, thetaDot, vyDot, rDot];
