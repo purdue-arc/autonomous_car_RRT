b7877ad77a8611355ba0d16453d1ba94951ecbc2
baxterjr@outlook.com <baxterjr@outlook.com>
2018-11-12 10:47:57 -0500

    corner state

--- RRT/testRRT.m
+++ RRT/testRRT.m
@@ -2,20 +2,20 @@ close all
 clear variables
 
 % Arbitrary values for the test
-x_min = -2;
-x_max = 2;
-y_min = -2;
-y_max = 2;
+x_min = 0;
+x_max = 10;
+y_min = 0;
+y_max = 10;
 
 figure
 hold on
-state = [0, 0, 0, 0, 0]; % [x CG, y CG, theta, lateral speed(vy), yaw rate(r or thetadot)]
+state = [0.5, 0.5, pi/4, 0, 0]; % [x CG, y CG, theta, lateral speed(vy), yaw rate(r or thetadot)]
 
 %tree(1,:) = cat(2, state, initial_x)
 state_tree(1,:) = state;
 parents = 0;
 
-for i = 1:2500
+for i = 2:2500
     % Create a new random position in the map
     rand_pos = [rand(1) * (x_max - x_min) + x_min, rand(1) * (y_max - y_min) + y_min];
     
