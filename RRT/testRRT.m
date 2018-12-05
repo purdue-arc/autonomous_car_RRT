bbb01a32021f7126397b848ea53698095373cf8b
baxterjr@outlook.com <baxterjr@outlook.com>
2018-12-05 17:44:55 -0500

    puts out images too

--- RRT/testRRT.m
+++ RRT/testRRT.m
@@ -10,7 +10,8 @@ radius = 0.25;
 
 % Plot / video export values
 plot_result = true;
-create_video = false;
+create_video = true;
+create_imgs = true;
 
 state = [0.5, 0.5, pi/4, 0, 0]; % [x CG, y CG, theta, lateral speed(vy), yaw rate(r or thetadot)]
 
@@ -27,7 +28,8 @@ for i = 2:5000
 end
 
 % Find path
-goal = [rand(1) * (x_max - x_min - 2*radius) + x_min + radius, rand(1) * (y_max - y_min - 2*radius) + y_min + radius];
+%goal = [rand(1) * (x_max - x_min - 2*radius) + x_min + radius, rand(1) * (y_max - y_min - 2*radius) + y_min + radius];
+goal = [8.3785, 5.7273];
 [path, length] = evaluateTree(state_tree, parents, goal, radius);
 
 % Plot for debugging / video export
@@ -43,16 +45,16 @@ if plot_result
         open(vid);
     end
     
-    plot_array = [;];
+    plot_array = [];
     for i = 1:size(state_tree, 1)
         curr_state = state_tree(i,:);
         new_point = plot(curr_state(1), curr_state(2), '*');
-        plot_array(1,i) = new_point; % Add
+        plot_array(i,1) = new_point; % Add
         % If it has a parent, plot a line
         if parents(i) ~= 0
             curr_parent = state_tree(parents(i),:);
             new_line = line([curr_state(1), curr_parent(1)], [curr_state(2), curr_parent(2)], 'Color', 'blue', 'LineStyle',':');
-            plot_array(2,i) = new_line;
+            plot_array(i,2) = new_line;
         end
         if create_video
             frame = getframe(gcf);
@@ -75,12 +77,12 @@ if plot_result
         index = path(i);
         curr_state = state_tree(index, 1:2);
         plot(curr_state(1), curr_state(2), '*r');
-        delete(plot_array(1,i));
+        set(plot_array(i,1),'Visible','off');
         % If it has a parent, plot a line
         if parents(index) ~= 0
             curr_parent = state_tree(parents(index),1:2);
             line([curr_state(1), curr_parent(1)], [curr_state(2), curr_parent(2)], 'Color', 'red');
-            delete(plot_array(2,i));
+            set(plot_array(i,2),'Visible','off');
         end
         if create_video
             frame = getframe(gcf);
@@ -93,5 +95,18 @@ if plot_result
 
     if create_video
         close(vid);
+        if create_imgs
+            % Export the final frame as an image too
+            [Image, Map] = frame2im(getframe(gcf));
+            imwrite(Image, 'RRT_example.png');
+            for i = 1:size(plot_array,1)
+                set(plot_array(i,1),'Visible','off');
+                if parents(i) ~= 0
+                    set(plot_array(i,2),'Visible','off');
+                end
+            end
+            [Image, Map] = frame2im(getframe(gcf));
+            imwrite(Image, 'RRT_example_path_only.png');
+        end
     end
 end
