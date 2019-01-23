6cf4aef8c7f07171268d2733601b0e3913664208
James Baxter <baxterjr@outlook.com>
2019-01-22 23:26:04 -0500

    frame rate of 6 doesn't seem to work, and walls appear to be handled poorly

--- RRT/testExplore.m
+++ RRT/testExplore.m
@@ -6,7 +6,9 @@ x_min = 0;
 x_max = 10;
 y_min = 0;
 y_max = 10;
-num_steps = 10;    % When to stop exploring (in future use while loop?)
+num_steps = 120;    % When to stop exploring (in future use while loop?)
+
+create_video = true;
 
 % Values for exploration
 simple_map = [0 0 0 0 1;
@@ -36,6 +38,38 @@ value_tree = zeros(num_steps, 1);   % The value of each move (prediction)
 state_tree(1,:) = cur_state;
 cur_view = map.execute_state(cur_state);
 
+% Initial plot
+set(gcf, 'Position', [0 0 1280 720]);
+clf;                                                        % Clear old stuff (since it can hang slightly off screen)
+colormap(flipud(gray));
+ax = subplot(1,2,1);                                        % Left plot
+hold on;
+axis([x_min x_max y_min y_max], 'square');                  % Set axis
+                                                            % Plot image
+imagesc('XData',[x_min+1/(scale*2) x_max-1/(scale*2)],'YData',[y_max-1/(scale*2) y_min+1/(scale*2)],'CData',map.obstacle_array);
+ax.ColorOrderIndex = 1;                                     % Make vis blue for consistency
+scatter(cur_view(:,1), cur_view(:,2), round(cur_view(:,3)*9)+1);       % Visibility
+scatter(cur_state(1), cur_state(2), 'filled');              % Car
+
+ax = subplot(1,2,2);                                        % Right plot
+hold on;
+axis([x_min x_max y_min y_max], 'square');                  % Set axis
+                                                            % Plot image
+imagesc('XData',[x_min+1/(scale*2) x_max-1/(scale*2)],'YData',[y_max-1/(scale*2) y_min+1/(scale*2)],'CData',map.observation_array);
+ax.ColorOrderIndex = 2;                                     % Get some nice orange
+scatter(cur_state(1), cur_state(2), 100, 'filled');       % Car
+
+drawnow;
+
+if create_video
+    set(gcf,'menubar','none')
+    vid = VideoWriter('Exploration_example', 'MPEG-4');
+    vid.FrameRate = 12;
+    open(vid);
+    frame = getframe(gcf);
+    writeVideo(vid, frame);
+end
+
 % Perform exploration
 for i = 2:num_steps
     % Choose next path
@@ -51,9 +85,9 @@ for i = 2:num_steps
     cur_view =  map.execute_state(next_state);
     
     % Update the graphs
-    set(gcf, 'Position', [0 0 1280 720]);
+%     set(gcf, 'Position', [0 0 1280 720]);
     clf;                                                        % Clear old stuff (since it can hang slightly off screen)
-    colormap(flipud(gray));
+%     colormap(flipud(gray));
     ax = subplot(1,2,1);                                        % Left plot
     hold on;
     axis([x_min x_max y_min y_max], 'square');                  % Set axis
@@ -73,6 +107,19 @@ for i = 2:num_steps
     scatter(next_state(1), next_state(2), 100, 'filled');       % Car
     
     drawnow;
+    
+    if create_video
+        frame = getframe(gcf);
+        writeVideo(vid, frame);
+        fprintf('wrote');
+        if mod(i,10) == 0
+            fprintf('progress: %d / %d\n', i, num_steps);
+        end
+    end
+end
+
+if create_video
+    close(vid);
 end
 
 % Display the map
