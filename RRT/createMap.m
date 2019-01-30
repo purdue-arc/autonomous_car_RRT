da3f9c51b5b16d6edd9f67f51ea4018c635f0d0e
baxterjr@outlook.com <baxterjr@outlook.com>
2019-01-29 23:00:42 -0500

    new map and way of making it

--- RRT/createMap.m
+++ RRT/createMap.m
@@ -5,9 +5,8 @@ close all;
 
 n_rows = 100;
 n_cols = 100;
-initial_obstacles = 0.95;
-min_obstacles = 0.075;
-max_obstacles = 0.75;
+initial_obstacles = 1-50/10000;
+branch_probability = 0.90;
 n_free = 2;
 filename = "100_map.mat";
 
@@ -16,44 +15,49 @@ rand_matrix = rand(n_rows, n_cols);
 subplot(2,2,1);
 imshow(1-rand_matrix, 'InitialMagnification', 'fit');
 
-obstacle_matrix_read = rand_matrix >= initial_obstacles;
-obstacle_matrix_mod = obstacle_matrix_read;
+obstacle_matrix = rand_matrix >= initial_obstacles;
+[obstacle_rows, obstacle_cols] = find(obstacle_matrix);
 
 subplot(2,2,3);
-imshow(~obstacle_matrix_read, 'InitialMagnification', 'fit');
-%free = find(rand_matrix < 0.25);
+imshow(~obstacle_matrix, 'InitialMagnification', 'fit');
 
-[r, c] = find((rand_matrix >= min_obstacles) & (rand_matrix < max_obstacles));
-possible_obstacles = [r, c];
+neighbors = [0, 1;
+             0, -1;
+             1, 0;
+            -1, 0;
+             1, 1;
+             1, -1;
+            -1, 1;
+            -1, -1];
 
-neighbors = [0 1;
-             0 -1;
-             1 0;
-            -1 0
-             1 1
-             1 -1
-            -1 1
-            -1 -1];
-
-for i=1:size(possible_obstacles)
-    row = possible_obstacles(i,1);
-    col = possible_obstacles(i,2);
-	num_neighbors = 0;
-    for j=1:size(neighbors,1)
-        r = neighbors(j,1) + row;
-        c = neighbors(j,2) + col;
-        if r > 0 && r <= n_rows && c > 0 && c <= n_cols
-            % we can check if this is good
-            num_neighbors = num_neighbors + obstacle_matrix_read(r,c);
+for i=1:size(obstacle_rows)
+    cur_row = obstacle_rows(i);
+    cur_col = obstacle_cols(i);
+    while rand <= branch_probability
+        rand_neighbor = round(rand * (size(neighbors,1)-1) + 1);
+        delta_row = neighbors(rand_neighbor, 1);
+        delta_col = neighbors(rand_neighbor, 2);
+        
+        new_row = delta_row + cur_row;
+        new_col = delta_col + cur_col;
+        
+        % Check if it is valid
+        if new_row > 0 && new_row <= n_rows && new_col > 0 && new_col <= n_cols
+            % New location is valid, add it in then build off of it
+            obstacle_matrix(new_row, new_col) = 1;
+            cur_row = new_row;
+            cur_col = new_col;
+            % Loop through again
+        else
+            % Looks like we found a dead end, go to next possible obstacle
+            break;
         end
     end
-    if num_neighbors/size(neighbors,1) >= rand_matrix(row, col)
-        obstacle_matrix_mod(row, col) = 1;
-    end
+
 end
 
 subplot(1,2,2);
-imshow(~obstacle_matrix_mod, 'InitialMagnification', 'fit');
+imshow(~obstacle_matrix, 'InitialMagnification', 'fit');
 
-obstacle_matrix_mod(1:n_free, 1:n_free) = zeros(n_free);
-save(filename, "obstacle_matrix_mod");
+obstacle_matrix(1:n_free, 1:n_free) = zeros(n_free);
+save(filename, "obstacle_matrix");
