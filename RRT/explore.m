294bd062fd2d96e3ca0dfe72635f553a64eb78e5
baxterjr@outlook.com <baxterjr@outlook.com>
2019-02-05 22:56:52 -0500

    removing distance made the performance much better

--- RRT/explore.m
+++ RRT/explore.m
@@ -16,31 +16,31 @@ function [next_state, next_control, next_value, state_tree, parents] = explore(m
 
     % Create knowledge and cost array
     knowledge_tree = zeros(num_nodes, 2); % Col 1: self knowledge, col 2: knowledge of self + children
-    distance_tree = zeros(num_nodes, 2);    % Col 1:  distance from parent, col 2: Distance of self + children
+    %distance_tree = zeros(num_nodes, 2);    % Col 1:  distance from parent, col 2: Distance of self + children
 
     % Compute knowledge and cost
     for i = num_nodes:-1:2 % Work backwards through tree
         parent_index = parents(i);
         % Calc knowledge and distance values
         knowledge = map.evaluate_state(state_tree(i,:));
-        x_dist = (state_tree(i,1) - state_tree(parent_index,1));
-        y_dist = (state_tree(i,2) - state_tree(parent_index,2));
-        distance = sqrt(x_dist^2+y_dist^2);
+%         x_dist = (state_tree(i,1) - state_tree(parent_index,1));
+%         y_dist = (state_tree(i,2) - state_tree(parent_index,2));
+%         distance = sqrt(x_dist^2+y_dist^2);
         
         % Update self
         knowledge_tree(i,:) = knowledge_tree(i,:) + [knowledge, knowledge];
-        distance_tree(i,:) = distance_tree(i,:) + [distance, distance];
+        %distance_tree(i,:) = distance_tree(i,:) + [distance, distance];
 
         % Update parent
         knowledge_tree(parent_index,2) = knowledge_tree(parent_index,2) + knowledge_tree(i,2);              % Add self + children knowledge to parent
-        distance_tree(parent_index,2) = distance_tree(parent_index,2) + distance + distance_tree(i,2);      % Add self + children distance to parent
+        %distance_tree(parent_index,2) = distance_tree(parent_index,2) + distance + distance_tree(i,2);      % Add self + children distance to parent
     end
 
     % Determine distance weight
-    distance_weight = knowledge_tree(1,2) / distance_tree(1,2);
+    %distance_weight = knowledge_tree(1,2) / distance_tree(1,2);
 
     % Determine value of each node
-    value_tree = knowledge_tree(:,2) + distance_weight * distance_tree(:,2);
+    value_tree = knowledge_tree(:,2);% + distance_weight * distance_tree(:,2);
 
     % Determine the indices of the possible next states
     possible_states = find(parents == 1);
